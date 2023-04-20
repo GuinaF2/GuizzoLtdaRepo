5f095b6b8dddd6ba48ea83a9a713a43a21cf41f8
GuinaF2 <guifo2.henrique@gmail.com>
2023-04-19 21:25:14 -0300

    Formatação Tosinus na biblioteca UsuarioModelo e UsuarioControle

--- Controle/UsuarioControle.cs
+++ Controle/UsuarioControle.cs
@@ -1,6 +1,141 @@
-﻿namespace Controle
+﻿using Modelos;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+
+namespace Controle
 {
-    public class UsuarioControle
+    public class UsuarioControle : UsuarioModelo
     {
+        Conexao conexaosql = new Conexao();
+        bool resultado;
+        public UsuarioControle()
+        {
+        }
+        public int cadastrar(UsuarioModelo modelo)
+        {
+            //variável de confirmação do banco
+            int valorCadastro = -1;
+            try
+            {
+                string SQL = "INSERT INTO tb_usuario(nome, cpf, email, telefone, senha) values(@nome, @cpf, @email, @telefone, @senha)";
+                //declaração de vetor de campos
+                string[] campos = { "@nome", "@cpf", "@email", "@telefone", "@senha" };
+                //declaração de vetor de informações
+                string[] valores = { modelo.NomeUser, modelo.CpfUser, modelo.EmailUser, modelo.FoneUser, modelo.SenhaUser };
+
+                if (conexaosql.cadastrar(campos, valores, SQL) >= 1)
+                {
+                    valorCadastro = 1;
+                }
+                else
+                {
+                    valorCadastro = 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                //Lança erros do sistema
+                throw new Exception(ex.Message);
+            }
+            return valorCadastro;
+        }
+        public bool DeletarUsuario(UsuarioModelo us)
+        {
+            try
+            {
+                string sql = "DELETE from tb_usuario where idusuario = @codigo";
+                if (conexaosql.deletarDados(us.CodUsuario, sql) >= 1)
+                {
+                    return resultado = true;
+                }
+                else
+                {
+                    return resultado = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public bool AtualizarUsuario(UsuarioModelo us)
+        {
+            try
+            {
+                string sql = "UPDATE tb_usuario set nome=@nome, cpf=@cpf, email=@email, telefone=@telefone, senha=@senha where idusuario= @codigo";
+                string[] campos = { "@nome", "@cpf", "@email", "@telefone", "@senha" };
+                string[] valores = { us.NomeUser, us.CpfUser, us.EmailUser, us.FoneUser, us.SenhaUser };
+                if (conexaosql.atualizarDados(us.CodUsuario, campos, valores, sql) >= 1)
+                {
+                    return resultado = true;
+                }
+                else
+                {
+                    return resultado = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public int logar(UsuarioModelo us)
+        {
+            int registro;
+            try
+            {
+                string sql = "SELECT COUNT(*) FROM tb_usuario where email=@email and senha=@senha";
+                MySqlConnection conexaologin = conexaosql.getConexao();
+                conexaologin.Open();
+
+                MySqlCommand cmd = new MySqlCommand(sql, conexaologin);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@email", us.EmailUser);
+                cmd.Parameters.AddWithValue("@senha", us.SenhaUser);
+                registro = Convert.ToInt32(cmd.ExecuteScalar()); //retorna um inteiro
+                conexaologin.Close();
+                return registro;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public UsuarioModelo CarregaUsuario(int id)
+        {
+            UsuarioModelo us = new UsuarioModelo();
+            try
+            {
+                conexaosql = new Conexao();
+                MySqlConnection con = conexaosql.getConexao();
+                con.Open();
+
+                MySqlCommand cmd = con.CreateCommand();
+
+                cmd.CommandText = "SELECT * from tb_usuario where idusuario = @id";
+                cmd.Parameters.AddWithValue("@id", id);
+
+                MySqlDataReader registro = cmd.ExecuteReader();
+
+                if (registro.HasRows)
+                {
+                    registro.Read();
+                    us.CpfUser = registro["cpf"].ToString();
+                    us.EmailUser = registro["email"].ToString();
+                    us.NomeUser = registro["nome"].ToString();
+                    us.FoneUser = registro["telefone"].ToString();
+                    us.SenhaUser = registro["senha"].ToString();
+                }
+                con.Close();
+            }
+            catch (System.Exception ex)
+            {
+                throw new System.Exception(ex.Message);
+            }
+            return us;
+        }
     }
 }
--- Modelos/UsuarioModelo.cs
+++ Modelos/UsuarioModelo.cs
@@ -2,5 +2,59 @@
 {
     public class UsuarioModelo
     {
+        //criação das variaveis protegidas
+        private int idUsuario;
+        private string cpf;
+        private string email;
+        private string nome;
+        private string telefone;
+        private string senha;
+        //defino o construtor modelo usuario
+
+        public UsuarioModelo()
+        {
+            idUsuario = 0;
+            cpf = "";
+            email = "";
+            telefone = "";
+            nome = "";
+            senha = "";
+        }
+
+        public int CodUsuario
+        {
+            get { return idUsuario; }
+            set { idUsuario = value; }
+
+        }
+        public string CpfUser
+        {
+            get { return cpf; }
+            set { cpf = value; }
+        }
+        public string EmailUser
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public string NomeUser
+        {
+            get { return nome; }
+            set { nome = value; }
+
+        }
+        public string FoneUser
+        {
+            get { return telefone; }
+            set { telefone = value; }
+
+        }
+        public string SenhaUser
+        {
+            get { return senha; }
+            set { senha = value; }
+
+        }
+
     }
 }
