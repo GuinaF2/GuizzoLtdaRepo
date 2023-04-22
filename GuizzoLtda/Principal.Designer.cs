c2d63a04549b48da5f3834664e537ea56c805b2b
GuinaF2 <guifo2.henrique@gmail.com>
2023-04-21 23:24:11 -0300

    Adição de Botões/ Formularios para Ambas telas Adm e Usuario

--- GuizzoLtda/Principal.Designer.cs
+++ GuizzoLtda/Principal.Designer.cs
@@ -28,48 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnUsuario = new System.Windows.Forms.Button();
+            this.btnAdmin = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnUsuario
+            // 
+            this.btnUsuario.Location = new System.Drawing.Point(459, 76);
+            this.btnUsuario.Name = "btnUsuario";
+            this.btnUsuario.Size = new System.Drawing.Size(285, 98);
+            this.btnUsuario.TabIndex = 0;
+            this.btnUsuario.Text = "Usuario";
+            this.btnUsuario.UseVisualStyleBackColor = true;
+            this.btnUsuario.Click += new System.EventHandler(this.btnUsuario_Click);
+            // 
+            // btnAdmin
+            // 
+            this.btnAdmin.Location = new System.Drawing.Point(459, 265);
+            this.btnAdmin.Name = "btnAdmin";
+            this.btnAdmin.Size = new System.Drawing.Size(285, 96);
+            this.btnAdmin.TabIndex = 1;
+            this.btnAdmin.Text = "Admin";
+            this.btnAdmin.UseVisualStyleBackColor = true;
+            this.btnAdmin.Click += new System.EventHandler(this.btnAdmin_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(248, 130);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 189);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nome";
+            this.label1.Size = new System.Drawing.Size(408, 54);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Selecione seu Usuário";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(248, 175);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(26, 15);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Cpf";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(248, 234);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(39, 15);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Senha";
-            // 
-            // Cadastro
+            // Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
-            this.Name = "Cadastro";
-            this.Text = "Form1";
+            this.Controls.Add(this.btnAdmin);
+            this.Controls.Add(this.btnUsuario);
+            this.Name = "Principal";
+            this.Text = "Selecione Usuario";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -77,8 +80,8 @@
 
         #endregion
 
+        private Button btnUsuario;
+        private Button btnAdmin;
         private Label label1;
-        private Label label4;
-        private Label label2;
     }
 }
--- GuizzoLtda/Principal.cs
+++ GuizzoLtda/Principal.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualBasic.Logging;
+
 namespace GuizzoLtda
 {
     public partial class Principal : Form
@@ -5,6 +7,20 @@ namespace GuizzoLtda
         public Principal()
         {
             InitializeComponent();
+        }
+
+        private void btnAdmin_Click(object sender, EventArgs e)
+        {
+            AdminCadLog fadmincad = new AdminCadLog();
+            this.Hide();
+            fadmincad.Show();
+        }
+
+        private void btnUsuario_Click(object sender, EventArgs e)
+        {
+            UsuarioCadLog fusercad = new UsuarioCadLog();
+            this.Hide();
+            fusercad.Show();
         }
     }
 }
