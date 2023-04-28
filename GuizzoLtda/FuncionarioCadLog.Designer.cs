0482695f22d2fd10f98db3ae383568913290da9d
GuinaF2 <guifo2.henrique@gmail.com>
2023-04-27 21:43:20 -0300

    Alteração do Formulario Login para formulario Teste

--- GuizzoLtda/FuncionarioCadLog.Designer.cs
+++ GuizzoLtda/FuncionarioCadLog.Designer.cs
@@ -31,17 +31,19 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtLogin = new System.Windows.Forms.TextBox();
-            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.txtCpf = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.btnCadSenha = new System.Windows.Forms.Button();
             this.btnLoginFunc = new System.Windows.Forms.Button();
+            this.txtContato = new System.Windows.Forms.TextBox();
+            this.txtDtNasc = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(309, 155);
+            this.label1.Location = new System.Drawing.Point(309, 101);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(40, 15);
             this.label1.TabIndex = 0;
@@ -50,75 +52,91 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(309, 234);
+            this.label2.Location = new System.Drawing.Point(309, 160);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(39, 15);
+            this.label2.Size = new System.Drawing.Size(28, 15);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Senha";
+            this.label2.Text = "CPF";
             // 
             // txtLogin
             // 
-            this.txtLogin.Location = new System.Drawing.Point(309, 173);
+            this.txtLogin.Location = new System.Drawing.Point(309, 119);
             this.txtLogin.Name = "txtLogin";
             this.txtLogin.Size = new System.Drawing.Size(133, 23);
             this.txtLogin.TabIndex = 2;
             // 
-            // txtSenha
+            // txtCpf
             // 
-            this.txtSenha.Location = new System.Drawing.Point(309, 252);
-            this.txtSenha.Name = "txtSenha";
-            this.txtSenha.Size = new System.Drawing.Size(133, 23);
-            this.txtSenha.TabIndex = 3;
-            this.txtSenha.TextChanged += new System.EventHandler(this.txtSenha_TextChanged);
+            this.txtCpf.Location = new System.Drawing.Point(309, 178);
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(133, 23);
+            this.txtCpf.TabIndex = 3;
+            this.txtCpf.TextChanged += new System.EventHandler(this.txtSenha_TextChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(329, 38);
+            this.label3.Location = new System.Drawing.Point(284, 21);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(113, 50);
+            this.label3.Size = new System.Drawing.Size(169, 50);
             this.label3.TabIndex = 4;
-            this.label3.Text = "Login";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(91, 181);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(97, 15);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "Primeiro Acesso?";
-            // 
-            // btnCadSenha
-            // 
-            this.btnCadSenha.Location = new System.Drawing.Point(76, 215);
-            this.btnCadSenha.Name = "btnCadSenha";
-            this.btnCadSenha.Size = new System.Drawing.Size(133, 39);
-            this.btnCadSenha.TabIndex = 6;
-            this.btnCadSenha.Text = "Criar Senha";
-            this.btnCadSenha.UseVisualStyleBackColor = true;
-            this.btnCadSenha.Click += new System.EventHandler(this.btnCadSenha_Click);
+            this.label3.Text = "Cadastro";
             // 
             // btnLoginFunc
             // 
-            this.btnLoginFunc.Location = new System.Drawing.Point(309, 308);
+            this.btnLoginFunc.Location = new System.Drawing.Point(309, 358);
             this.btnLoginFunc.Name = "btnLoginFunc";
             this.btnLoginFunc.Size = new System.Drawing.Size(133, 39);
             this.btnLoginFunc.TabIndex = 7;
             this.btnLoginFunc.Text = "Criar Senha";
             this.btnLoginFunc.UseVisualStyleBackColor = true;
+            this.btnLoginFunc.Click += new System.EventHandler(this.btnLoginFunc_Click);
+            // 
+            // txtContato
+            // 
+            this.txtContato.Location = new System.Drawing.Point(309, 289);
+            this.txtContato.Name = "txtContato";
+            this.txtContato.Size = new System.Drawing.Size(133, 23);
+            this.txtContato.TabIndex = 11;
+            // 
+            // txtDtNasc
+            // 
+            this.txtDtNasc.Location = new System.Drawing.Point(309, 230);
+            this.txtDtNasc.Name = "txtDtNasc";
+            this.txtDtNasc.Size = new System.Drawing.Size(133, 23);
+            this.txtDtNasc.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(309, 271);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Contato";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(309, 212);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Data Nascimento";
             // 
             // FuncionarioCadLog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtContato);
+            this.Controls.Add(this.txtDtNasc);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.btnLoginFunc);
-            this.Controls.Add(this.btnCadSenha);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtCpf);
             this.Controls.Add(this.txtLogin);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -134,10 +152,12 @@
         private Label label1;
         private Label label2;
         private TextBox txtLogin;
-        private TextBox txtSenha;
+        private TextBox txtCpf;
         private Label label3;
-        private Label label4;
-        private Button btnCadSenha;
         private Button btnLoginFunc;
+        private TextBox txtContato;
+        private TextBox txtDtNasc;
+        private Label label5;
+        private Label label6;
     }
 }
--- GuizzoLtda/FuncionarioCadLog.cs
+++ GuizzoLtda/FuncionarioCadLog.cs
@@ -26,5 +26,10 @@ namespace GuizzoLtda
         {
 
         }
+
+        private void btnLoginFunc_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
