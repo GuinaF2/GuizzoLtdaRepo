13148a6a2c08290f066e81e55f05f301c8372fca
GuinaF2 <guifo2.henrique@gmail.com>
2023-05-06 11:26:14 -0300

    CadEmpresa formatado

--- GuizzoLtda/CadEmpresa.Designer.cs
+++ GuizzoLtda/CadEmpresa.Designer.cs
@@ -30,6 +30,22 @@
         {
             this.btnEscolherFotoEmp = new System.Windows.Forms.Button();
             this.pbFotoEmp = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRazaosoc = new System.Windows.Forms.TextBox();
+            this.txtCnpj = new System.Windows.Forms.TextBox();
+            this.txtIncEstad = new System.Windows.Forms.TextBox();
+            this.txtRua = new System.Windows.Forms.TextBox();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.txtCep = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtComple = new System.Windows.Forms.TextBox();
+            this.txtNumero = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pbFotoEmp)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,17 +70,162 @@
             this.pbFotoEmp.TabStop = false;
             this.pbFotoEmp.Click += new System.EventHandler(this.pbFotoEmp_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Razão Social";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Cnpj";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Inscrição Estadual";
+            // 
+            // txtRazaosoc
+            // 
+            this.txtRazaosoc.Location = new System.Drawing.Point(27, 55);
+            this.txtRazaosoc.Name = "txtRazaosoc";
+            this.txtRazaosoc.Size = new System.Drawing.Size(211, 23);
+            this.txtRazaosoc.TabIndex = 6;
+            // 
+            // txtCnpj
+            // 
+            this.txtCnpj.Location = new System.Drawing.Point(27, 114);
+            this.txtCnpj.Name = "txtCnpj";
+            this.txtCnpj.Size = new System.Drawing.Size(211, 23);
+            this.txtCnpj.TabIndex = 7;
+            // 
+            // txtIncEstad
+            // 
+            this.txtIncEstad.Location = new System.Drawing.Point(27, 189);
+            this.txtIncEstad.Name = "txtIncEstad";
+            this.txtIncEstad.Size = new System.Drawing.Size(211, 23);
+            this.txtIncEstad.TabIndex = 8;
+            // 
+            // txtRua
+            // 
+            this.txtRua.Location = new System.Drawing.Point(316, 55);
+            this.txtRua.Name = "txtRua";
+            this.txtRua.Size = new System.Drawing.Size(100, 23);
+            this.txtRua.TabIndex = 10;
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Location = new System.Drawing.Point(313, 110);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(100, 23);
+            this.txtBairro.TabIndex = 11;
+            // 
+            // txtCep
+            // 
+            this.txtCep.Location = new System.Drawing.Point(318, 169);
+            this.txtCep.Name = "txtCep";
+            this.txtCep.Size = new System.Drawing.Size(100, 23);
+            this.txtCep.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(313, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 15);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Rua";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(318, 92);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 15);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Bairro";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(313, 151);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 15);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Cep";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(313, 265);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(84, 15);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Complemento";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(318, 206);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(51, 15);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Numero";
+            // 
+            // txtComple
+            // 
+            this.txtComple.Location = new System.Drawing.Point(318, 283);
+            this.txtComple.Name = "txtComple";
+            this.txtComple.Size = new System.Drawing.Size(100, 23);
+            this.txtComple.TabIndex = 17;
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(313, 224);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 23);
+            this.txtNumero.TabIndex = 16;
+            // 
             // CadEmpresa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtComple);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtCep);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.txtRua);
+            this.Controls.Add(this.txtIncEstad);
+            this.Controls.Add(this.txtCnpj);
+            this.Controls.Add(this.txtRazaosoc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pbFotoEmp);
             this.Controls.Add(this.btnEscolherFotoEmp);
             this.Name = "CadEmpresa";
             this.Text = "CadEmpresa";
             ((System.ComponentModel.ISupportInitialize)(this.pbFotoEmp)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,5 +233,21 @@
 
         private Button btnEscolherFotoEmp;
         private PictureBox pbFotoEmp;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtRazaosoc;
+        private TextBox txtCnpj;
+        private TextBox txtIncEstad;
+        private TextBox txtRua;
+        private TextBox txtBairro;
+        private TextBox txtCep;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private TextBox txtComple;
+        private TextBox txtNumero;
     }
 }
